fe63b9d5de773e70ebb65377fc2591b060c20b79
Leonardo Anjos <leodrums11@gmail.com>
2023-12-21 22:37:31 -0300

    hanlder erros

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty input value")
+	}
+
+	message := fmt.Sprintf("Hi, %v. Welcome to go lang wolrd!", name)
+	return message, nil
 }
